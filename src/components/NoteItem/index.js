d463c2f4fdca3487a54b8d5cb41f85cbac147587
SrinivasPolaveni <srinivasyadavpolaveni@gmail.com>
2022-11-09 05:47:22 +0000

    Notes app

---
+++ src/components/NoteItem/index.js
@@ -0,0 +1,14 @@
+// Write your code here
+import {NotesListItem, Heading, Description} from './styledComponents'
+
+const NoteItem = props => {
+  const {noteDetails} = props
+  const {title, notesText} = noteDetails
+  return (
+    <NotesListItem>
+      <Heading>{title}</Heading>
+      <Description>{notesText}</Description>
+    </NotesListItem>
+  )
+}
+export default NoteItem
---
+++ src/components/NoteItem/styledComponents.js
@@ -0,0 +1,25 @@
+// Style your elements here
+import styled from 'styled-components'
+
+export const NotesListItem = styled.li`
+  min-height: 150px;
+  max-height: 200px;
+  width: 150px;
+  border: 0.1px solid #334155;
+  padding: 10px;
+  text-align: left;
+  margin-right: 10px;
+  border-radius: 8px;
+`
+
+export const Heading = styled.h1`
+  font-family: 'Roboto';
+  font-size: 20px;
+  color: #475569;
+`
+
+export const Description = styled.p`
+  font-family: 'Roboto';
+  font-size: 16px;
+  color: #475569;
+`
